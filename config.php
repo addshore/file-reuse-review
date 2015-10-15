206ebb88ac632779d35349151d6e1ef9df1e971c
addshore <addshorewiki@gmail.com>
2015-10-15 12:34:02 +0100

    Remove trailing space in config

--- config.php
+++ config.php
@@ -2,5 +2,5 @@
 return [
 	'debug' => true,
 	'swiftmailer.transport' => Swift_SendmailTransport::newInstance('/usr/sbin/sendmail -bs'),
-	'feedback_email' => 'tools.file-reuse@tools.wmflabs.org ', // the email address that receives the feedback
+	'feedback_email' => 'tools.file-reuse@tools.wmflabs.org', // the email address that receives the feedback
 ];
